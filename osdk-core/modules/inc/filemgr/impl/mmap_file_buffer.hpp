5f31ab7f1437240d6924674cbd12a36336fcac95
Julian Euler <jeuler@seemoo.tu-darmstadt.de>
2023-07-06 12:17:57 +0900

    mmap_file_buffer: add missing includes for build

--- osdk-core/modules/inc/filemgr/impl/mmap_file_buffer.hpp
+++ osdk-core/modules/inc/filemgr/impl/mmap_file_buffer.hpp
@@ -5,34 +5,37 @@
 #ifndef ONBOARDSDK_INTERNAL_RESOURCES_SRC_OSDK_CORE_MODULES_INC_FILEMGR_IMPL_MMAP_FILE_BUFFER_HPP_
 #define ONBOARDSDK_INTERNAL_RESOURCES_SRC_OSDK_CORE_MODULES_INC_FILEMGR_IMPL_MMAP_FILE_BUFFER_HPP_
 
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <memory>
 #include <atomic>
+#include <fcntl.h>
+#include <memory>
+#include <string>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-namespace DJI {
-namespace OSDK {
-class MmapFileBuffer {
- public:
+namespace DJI
+{
+namespace OSDK
+{
+class MmapFileBuffer
+{
+public:
   MmapFileBuffer();
   ~MmapFileBuffer();
 
   std::string currentLogFilePath;
-  int fd;
-  char *fdAddr;
-  uint64_t fdAddrSize;
-  uint64_t curFilePos;
+  int         fd;
+  char*       fdAddr;
+  uint64_t    fdAddrSize;
+  uint64_t    curFilePos;
 
   bool init(std::string path, uint64_t fileSize);
 
   bool deInit();
 
-  bool InsertBlock(const uint8_t *pack, uint32_t data_length, uint64_t index);
-
+  bool InsertBlock(const uint8_t* pack, uint32_t data_length, uint64_t index);
 };
 }
 }
-#endif //ONBOARDSDK_INTERNAL_RESOURCES_SRC_OSDK_CORE_MODULES_INC_FILEMGR_IMPL_MMAP_FILE_BUFFER_HPP_
+#endif // ONBOARDSDK_INTERNAL_RESOURCES_SRC_OSDK_CORE_MODULES_INC_FILEMGR_IMPL_MMAP_FILE_BUFFER_HPP_
--- osdk-core/modules/src/filemgr/impl/mmap_file_buffer.cpp
+++ osdk-core/modules/src/filemgr/impl/mmap_file_buffer.cpp
@@ -3,26 +3,39 @@
 //
 #include "mmap_file_buffer.hpp"
 #include "dji_log.hpp"
+#include <cstdio>
 #include <string.h>
 
-namespace DJI {
-namespace OSDK {
+namespace DJI
+{
+namespace OSDK
+{
 
-MmapFileBuffer::MmapFileBuffer() : fdAddr(NULL), curFilePos(0) {}
+MmapFileBuffer::MmapFileBuffer()
+  : fdAddr(NULL)
+  , curFilePos(0)
+{
+}
 
-MmapFileBuffer::~MmapFileBuffer() {}
+MmapFileBuffer::~MmapFileBuffer()
+{
+}
 
-bool MmapFileBuffer::init(std::string path, uint64_t fileSize) {
+bool
+MmapFileBuffer::init(std::string path, uint64_t fileSize)
+{
   currentLogFilePath = path;
-  fdAddrSize = fileSize;
-  curFilePos = 0;
+  fdAddrSize         = fileSize;
+  curFilePos         = 0;
   printf("Preparing File : %s\n", this->currentLogFilePath.c_str());
   fd = open(this->currentLogFilePath.c_str(), O_RDWR | O_CREAT, 0644);
   DSTATUS("fd = %d", fd);
-  if (fd < 0) return false;
+  if (fd < 0)
+    return false;
 
   ftruncate(fd, fdAddrSize);
-  fdAddr = (char *) mmap(NULL, fdAddrSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  fdAddr =
+    (char*)mmap(NULL, fdAddrSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
   if (fdAddr)
     return true;
@@ -30,23 +43,31 @@ bool MmapFileBuffer::init(std::string path, uint64_t fileSize) {
     return false;
 }
 
-bool MmapFileBuffer::deInit() {
+bool
+MmapFileBuffer::deInit()
+{
   DSTATUS("Deinit");
-  if (fd >= 0) close(fd);
+  if (fd >= 0)
+    close(fd);
   fd = -1;
-  if (fdAddr) munmap(fdAddr, fdAddrSize);
+  if (fdAddr)
+    munmap(fdAddr, fdAddrSize);
   fdAddr = NULL;
   return true;
 }
 
-
 // flag 代表是否覆盖已有队列缓存
-bool MmapFileBuffer::InsertBlock(const uint8_t *pack, uint32_t data_length, uint64_t index) {
+bool
+MmapFileBuffer::InsertBlock(const uint8_t* pack,
+                            uint32_t       data_length,
+                            uint64_t       index)
+{
 #if 0
   static uint32_t tempAdaptingBufferCnt = 0;
   if (index == 1) tempAdaptingBufferCnt = data_length;
 #endif
-  if ((data_length <= 0) || !fdAddr) {
+  if ((data_length <= 0) || !fdAddr)
+  {
     return false;
   }
 #if 0
