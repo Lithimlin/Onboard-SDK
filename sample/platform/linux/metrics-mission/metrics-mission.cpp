66977f506507df4cba3a458341d5d65892d35f29
Julian Euler <jeuler@seemoo.tu-darmstadt.de>
2023-08-03 18:58:52 +0900

    without extra pointer

--- sample/platform/linux/metrics-mission/metrics-mission.cpp
+++ sample/platform/linux/metrics-mission/metrics-mission.cpp
@@ -56,9 +56,10 @@ MetricsMission::MetricsMission(Vehicle*            vehiclePtr,
   std::cout << "Center point is (LLA)" << std::endl
             << waypoint_to_string(centerPoint) << std::endl;
 
-  std::cout << "Callback " << &waypointEventCallback << std::endl;
+  std::cout << "Callback " << waypointEventCallback << std::endl;
+
   vehiclePtr->missionManager->wpMission->setWaypointCallback(
-    &waypointEventCallback, vehiclePtr);
+    waypointEventCallback, vehiclePtr);
 
   std::cout << "Starting metrics timer..." << std::endl;
   metricsTimer.async_wait(boost::bind(
